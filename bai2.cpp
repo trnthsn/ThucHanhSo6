a0fdceb3e8df4feca45701b4337e4a20153cf290
TrnThSn <saophaisoan23112002@gmail.com>
2021-03-17 23:22:49 +0700

    bài 2 đã test

---
+++ bai2.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+
+int ucln(int a, int b)
+{
+    if (b == 0)
+        return a;
+    else
+        return ucln(b, a % b);
+}
+
+bool check(int a[], int b, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] % b)
+            return false;
+    }
+    return true;
+}
+
+void write(int *a, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+}
+
+int bcnn(int a, int b)
+{
+    return (a * b) / ucln(a, b);
+}
+
+int main()
+{
+    int m, n;
+    int count = 0;
+    cin >> m >> n;
+    int a[1000], b[1000];
+    write(a, m);
+    write(b, n);
+    // tìm bội chung nhỏ nhất của mảng thứ nhất
+    int bcnn1 = a[0];
+    for (int i = 0; i < m; i++)
+    {
+    	bcnn1 = bcnn(bcnn1, a[i]);
+    }
+    // tìm ước chung lớn nhất của mảng thứ 2
+    int ucln1 = b[0];
+    for (int i = 0; i < n; i++)
+    {
+    	ucln1 = ucln(ucln1, b[i]);
+    }
+    // check các số thoả mãn yêu cầu đề bài
+    for (int i = bcnn1; i <= ucln1; i++)
+    {
+    	if (check(b, i, n))
+    	{
+    		count++;
+    	}
+    }
+    cout << count;
+}
